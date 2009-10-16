143654862681ff7e4f7483ed1afaa02ed1832aa7
Christophe Alladoum <ca@hsc.fr>
2009-10-16 14:05:14 +0200

    simplification du button handler, une touche correspond a un code (et non plus un string).

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
+
+__author__ = "hugsy"
+__version__ = 0.2
+__licence__ = "WTFPL v.2"
+__file__ = "/home/chris/code/pywii/pywii.py"
+__doc__ = """ 
+
+This simple tool was basically written because I'm lazy,
+and is purposed to make a bit of fun with your wiimote by
+(simply) interacting with your system. Some events (buttons
+pressing, wiimote moves) can be match to perform a specific
+action on your system.
+
+Up to 4 Wiimotes can be used simultaneously (but I don't see
+the point in that :) ).
+
+All suggestions welcomed.
+
 """ 
-
-Simple tool to make a bit of fun with your wiimote.
-Can be used to interact with your system !
-
-""" 
-
-__author__="hugsy"
-__version__=0.1
-__licence__="WTFPL v.2"
-__file__="/home/chris/code/pywii/pywii.py"
 
 from config import DEBUG
 from lib.wiimote import Wiimote
@@ -17,30 +25,28 @@ from lib.base import find_wiimotes
 
 if __name__ == "__main__":
     if DEBUG :
-        wiimote_addrs = [('00:19:1D:B7:43:0D', 'Nintendo RVL-CNT-01')]        
+        wiimote_addrs = [('00:19:1D:B7:43:0D', 'Nintendo RVL-CNT-01')]
     else :
         wiimote_addrs = find_wiimotes()
 
     if len(wiimote_addrs) == 0 :
-        print "No Wiimote found"
+        print ("No Wiimote found")
         exit (129)
         
     wii_num = 1
     wiimote_threads = []
     
     for wm in wiimote_addrs:
-        if DEBUG: print "Creating Wiimote-%d" % wii_num
+        print ("Creating Wiimote-%d" % wii_num)
         wiimote = Wiimote(str(wm[0]), str(wm[1]), wii_num)
         wiimote.start()
         wiimote_threads.append(wiimote)
         
         wii_num += 1
         if wii_num > 4 :
-            print "Cannot handle more than 4 Wiimotes"
+            print ("Cannot handle more than 4 Wiimotes")
             exit (130)
 
-    if wii_num > 1:
-        for wm in wiimote_threads :
-            wm.join()
+    for wm in wiimote_threads : wm.join()
     
     exit (0)
